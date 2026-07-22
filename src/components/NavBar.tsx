2eb232b88488a488aec921b610dd52fb5d6a0162
jamel salah <jamelsala@gmail.com>
2026-07-22 16:58:28 -0300

    style: estiliza a barra de navegacao

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,27 @@
 import { NavLink } from 'react-router-dom'
 
+import styles from './NavBar.module.css'
+
+// O NavLink entrega { isActive }; devolvo a classe conforme a aba atual.
+// if/else (não ternário) para seguir o padrão do projeto.
+function navLinkClass({ isActive }: { isActive: boolean }) {
+  if (isActive) {
+    return `${styles.link} ${styles.active}`
+  }
+  return styles.link
+}
+
 export function NavBar() {
   return (
-    <nav>
-      <NavLink to="/" end>
-        Episódios
-      </NavLink>
-      <NavLink to="/favoritos">
-        Favoritos
-      </NavLink>
+    <nav className={styles.nav}>
+      <div className={styles.inner}>
+        <NavLink to="/" end className={navLinkClass}>
+          Episódios
+        </NavLink>
+        <NavLink to="/favoritos" className={navLinkClass}>
+          Favoritos
+        </NavLink>
+      </div>
     </nav>
   )
 }
