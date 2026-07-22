201e536bacceee4d3d259a74b2d5be4eced5bb3e
jamel salah <jamelsala@gmail.com>
2026-07-22 16:49:23 -0300

    feat: adiciona a rota de favoritos

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import { Layout } from '@/components/Layout'
 import { EpisodeDetailPage } from '@/pages/EpisodeDetailPage'
+import { FavoritesPage } from '@/pages/FavoritesPage'
 import { Home } from '@/pages/Home'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/favoritos" element={<FavoritesPage />} />
+        </Route>
         <Route path="/episodio/:id" element={<EpisodeDetailPage />} />
       </Routes>
     </BrowserRouter>
